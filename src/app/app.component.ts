59eaaf7b98de68c8d2d3df8f42b36caf51d75a23
Guy Ben-Aharon <baguy3@gmail.com>
2017-05-14 20:11:27 +0300

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Game} from "./models/Game";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  //title = 'app works!';
+  public game: Game;
+
+  constructor(){
+    this.game = new Game(300, 300, 5);
+    this.game.start();
+
+    // setTimeout(function() {
+    //   this.game = new Game(300, 300, 6);
+    //   this.game.initialize();
+    // }.bind(this), 1000);
+  }
 }
