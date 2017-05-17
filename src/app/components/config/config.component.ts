4490a8139a6e04cd454b5c804d57b1ba54da259f
Guy Ben-Aharon <baguy3@gmail.com>
2017-05-17 22:12:58 +0300

    Final

--- src/app/components/config/config.component.ts
+++ src/app/components/config/config.component.ts
@@ -8,7 +8,10 @@ import {MdDialogRef, MD_DIALOG_DATA} from "@angular/material";
   styleUrls: ['./config.component.css']
 })
 export class ConfigComponent implements OnInit {
+  readonly MAX_WIDTH: number = 300;
+  readonly MAX_HEIGHT: number = 300;
   public game: Game;
+
   constructor(public dialogRef: MdDialogRef<ConfigComponent>, @Inject(MD_DIALOG_DATA) public data: any) {
     this.game = data.game;
   }
@@ -16,4 +19,16 @@ export class ConfigComponent implements OnInit {
   ngOnInit() {
   }
 
+  public isWidthValid(): boolean{
+    return this.game.width <= this.MAX_WIDTH && this.game.width >= 1;
+  }
+
+  public isHeightValid(): boolean{
+    return this.game.height <= this.MAX_HEIGHT && this.game.height >= 1;
+  }
+
+  public isMinesNumValid(){
+    var maxMines = this.game.width * this.game.height;
+    return this.game.mines <= maxMines && this.game.mines >= 1;
+  }
 }
