d3b5b854bce4607815927869ec6a9e6bbe86b3c0
Guy Ben-Aharon <baguy3@gmail.com>
2017-05-18 14:02:20 +0300

    Adding tests

--- src/app/components/tile/tile.component.spec.ts
+++ src/app/components/tile/tile.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TileComponent } from './tile.component';
 import {FreeTile} from "../../models/FreeTile";
 import {MdIconModule} from "@angular/material";
+import {Mine} from "../../models/Mine";
 
 describe('TileComponent', () => {
   let component: TileComponent;
@@ -16,15 +17,46 @@ describe('TileComponent', () => {
     .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TileComponent);
-    component = fixture.componentInstance;
+  // beforeEach(() => {
+  //   fixture = TestBed.createComponent(TileComponent);
+  //   component = fixture.componentInstance;
+  // });
+
+  it('should be created', () => {
+    const fixture = TestBed.createComponent(TileComponent);
+    const component = fixture.componentInstance;
     component.tile = new FreeTile(1,1);
     component.globalParams = {};
     fixture.detectChanges();
+    expect(component).toBeTruthy();
   });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
+  it('should be mine', () => {
+    const fixture = TestBed.createComponent(TileComponent);
+    const component = fixture.componentInstance;
+    component.tile = new Mine(1);
+    component.globalParams = {};
+    fixture.detectChanges();
+    expect(component.isMine()).toBe(true);
+  });
+
+  it('should not be mine', () => {
+    const fixture = TestBed.createComponent(TileComponent);
+    const component = fixture.componentInstance;
+    component.tile = new FreeTile(1,1);
+    component.globalParams = {};
+    fixture.detectChanges();
+    expect(component.isMine()).toBe(false);
+  });
+
+  it('should be revealed', () => {
+    const fixture = TestBed.createComponent(TileComponent);
+    const component = fixture.componentInstance;
+    component.tile = new FreeTile(1,20);
+    component.tile.isRevealed = true;
+    component.globalParams = {};
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('.tile').textContent).toContain('20');
   });
 });
